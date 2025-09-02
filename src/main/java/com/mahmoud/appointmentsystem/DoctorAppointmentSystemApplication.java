81f0e96e97a141133b0256a9051b2282f24d96f4
Mahmoud Ramdan <mrd@corelia.ai>
2025-09-02 13:49:34 +0300

    Auto reload .dev file based on InetAddress of device dev or prod

--- src/main/java/com/mahmoud/appointmentsystem/DoctorAppointmentSystemApplication.java
+++ src/main/java/com/mahmoud/appointmentsystem/DoctorAppointmentSystemApplication.java
@@ -1,12 +1,18 @@
 package com.mahmoud.appointmentsystem;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import org.apache.catalina.util.ServerInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Properties;
+
 
 @SpringBootApplication
 @EnableFeignClients
@@ -22,11 +28,25 @@ public class DoctorAppointmentSystemApplication {
 	 * @param args command line arguments
 	 */
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) throws UnknownHostException {
+
+        //
+        //Properties properties = System.getProperties();
+		String inetAddress = InetAddress.getLocalHost().getHostName();
+        System.out.println("InetAddress: " + inetAddress);
+        // my Desktop
+		if (inetAddress.toUpperCase().equals("DESKTOP-7HGNMJC")) {
+            System.setProperty("spring.profiles.active", "dev");
+        } else  if (inetAddress.toUpperCase().equals("production server name")){
+            System.setProperty("spring.profiles.active", "prod");
+        }
+        // in above, we can specify dev or prod based on InetAddress
+
+
 		// auto Load environment variables from the.env file based on the active profile
 		String profile = System.getProperty("spring.profiles.active","dev");
-		logger.info("Active profile: {}", profile);
+
+				logger.info("Active profile: {}", profile);
 
 		Dotenv dotenv = Dotenv.configure()
 				.filename(".%s.env".formatted(profile))
