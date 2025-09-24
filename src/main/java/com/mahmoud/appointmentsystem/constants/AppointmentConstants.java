7dda9306bba09840647e1e0681c2b35aed6f5e3a
Mahmoud Ramdan <mrd@corelia.ai>
2025-09-24 17:09:56 +0300

    Add AppointmentConstants.java

---
+++ src/main/java/com/mahmoud/appointmentsystem/constants/AppointmentConstants.java
@@ -0,0 +1,20 @@
+package com.mahmoud.appointmentsystem.constants;
+
+public class AppointmentConstants {
+
+    public static final String STATUS_PENDING = "PENDING";
+    public static final String STATUS_BOOKED = "BOOKED";
+    public static final String STATUS_CANCELLED = "CANCELLED";
+    public static final String STATUS_COMPLETED = "COMPLETED";
+
+
+    public static final String EVENT_APPOINTMENT_PENDING = "APPOINTMENT_PENDING";
+    public static final String EVENT_APPOINTMENT_BOOKED = "APPOINTMENT_BOOKED";
+    public static final String EVENT_APPOINTMENT_CANCELLED = "APPOINTMENT_CANCELLED";
+    public static final String EVENT_APPOINTMENT_COMPLETED = "APPOINTMENT_COMPLETED";
+
+
+    public static final String ROLE_DOCTOR = "ROLE_DOCTOR";
+    public static final String ROLE_PATIENT = "ROLE_PATIENT";
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+}
